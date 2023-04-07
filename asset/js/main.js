e88a03fba05d8673b7bf6273b2dc28d4c5452629
bydzen <svnteentthones@gmail.com>
2023-04-07 13:38:03 +0700

    major changes v3

--- asset/js/main.js
+++ asset/js/main.js
@@ -3,10 +3,12 @@ window.onscroll = function () {
     showScrollButton()
 }
 function showScrollButton() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollTopButton.style.opacity = '1'
-    } else {
-        scrollTopButton.style.opacity = '0'
+    if (scrollTopButton) {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            scrollTopButton.style.opacity = '1'
+        } else {
+            scrollTopButton.style.opacity = '0'
+        }
     }
 }
 function scrollTopAction() {
