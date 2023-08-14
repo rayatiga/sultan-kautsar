3d51fce1c9965d72e14de9e6666e0108e2e24e04
bydzen <svnteentthones@gmail.com>
2023-08-14 09:04:01 +0700

    move to cdn rayatiga

--- public/asset/js/tooltip.js
+++
@@ -1,3 +0,0 @@
-// Tooltip
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl))
