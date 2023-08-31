5b59ceda96cf66230ab5cc52a90f7d10e4a88e5f
sultan <svnteentthones@gmail.com>
2023-08-31 16:40:22 +0700

    v1.5

--- public/asset/js/main.js
+++ public/asset/js/main.js
@@ -1,24 +1,24 @@
-/* ===== GLOBAL START ===== */
+/* ===== Global ===== */
 // Display title and subtitle inside header
-document.getElementById('hdr').outerHTML = `<h1>Sultan Kautsar</h1>
-                                            <p>Founder at Rayatiga, Business & Technology Enthusiast</p>`
+document.getElementById('header').outerHTML = `<h1>Sultan Kautsar</h1>
+                                               <p>Founder at Rayatiga, Business & Technology Enthusiast</p>`
 // Display navigation inside footer
-document.getElementById('map').outerHTML = `<p>Navigation & Sitemap</p>
-                                            <ul class="map">
-                                              <li><a href="/" title="Go to home page">Home</a></li>
-                                              <li><a href="/link.html" title="Go to link page">Link</a></li>
-                                              <li><a href="/sitemap.html" title="Go to sitemap page">Sitemap</a></li>
-                                            </ul>`
+document.getElementById('navigation-sitemap').outerHTML = `<p>Navigation & Sitemap</p>
+                                                           <ul class="navigation-sitemap">
+                                                             <li><a href="/" title="Go to home page">Home</a></li>
+                                                             <li><a href="/link.html" title="Go to link page">Link</a></li>
+                                                             <li><a href="/sitemap.html" title="Go to sitemap page">Sitemap</a></li>
+                                                           </ul>`
 // Display pathname location inside footer
-document.getElementById('loc').outerHTML = `<p>Location: ${window.location.pathname}</p>`
+document.getElementById('location').outerHTML = `<p>Location: ${window.location.pathname}</p>`
 // Display scheme inside footern
-document.getElementById('sme').outerHTML = `<p>Your native theme is <span id="nesme"></span>. Switch scheme to <span onclick="switchScheme()" class="sme"></span>.</p>`
+document.getElementById('scheme').outerHTML = `<p>Your native theme is <span id="native-scheme"></span>. Switch scheme to <span onclick="switchScheme()" class="scheme"></span>.</p>`
 // Display copyright inside footer
-document.getElementById('cr').outerHTML = `<p class="cr">Copyright &copy; 2020-2023 Sultan Kautsar. All Rights Reserved.</p>`
-// Get preffered color scheme, local storage scheme, button sme
+document.getElementById('copyright').outerHTML = `<p class="cr">Copyright &copy; 2020-2023 Sultan Kautsar. All Rights Reserved.</p>`
+// Get preffered color scheme, local storage scheme, button scheme
 let preferScheme = window.matchMedia('(prefers-color-scheme: light)').matches
 let currentScheme = localStorage.getItem('scheme')
-let nativeScheme = document.getElementById('nesme')
+let nativeScheme = document.getElementById('native-scheme')
 // Initial set scheme by user device theme
 if (currentScheme == null && preferScheme) {
   currentScheme = 'light'
@@ -47,4 +47,4 @@ function switchScheme() {
     toLight()
   }
 }
-/* ===== GLOBAL END ===== */
+/* ===== Global ===== */
