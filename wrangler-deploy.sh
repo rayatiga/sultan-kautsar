4296ed34b527ab42e51c33fd0dda4a70299718e7
sultan <svnteentthones@gmail.com>
2023-09-11 08:19:03 +0700

    v3.3

--- wrangler-deploy.sh
+++ wrangler-deploy.sh
@@ -1,3 +1,5 @@
 echo "Deploying to: sultankautsar"
 read -p "Enter commit message: " commitMessage
-wrangler pages deploy public/ --project-name "sultankautsar" --commit-message "$commitMessage"
+read -p "Enter directory to deploy: public/" directoryDeploy
+echo "Deploy from directory public/$directoryDeploy with commit message $commitMessage"
+wrangler pages deploy public/$directoryDeploy --project-name "sultankautsar" --commit-message "$commitMessage"
