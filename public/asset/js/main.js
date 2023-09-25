5e7c8154622c7c8298b41e3adeeacd1f72e1bfee
bydzen <svnteentthones@gmail.com>
2023-09-25 10:57:45 +0700

    v4.7

--- public/asset/js/main.js
+++ public/asset/js/main.js
@@ -39,20 +39,4 @@ function switchScheme() {
 function onLocal() {
   return window.location.hostname == '127.0.0.1'
 }
-// Developer navigation
-if (onLocal()) {
-  console.warn(`Window hostname is ${window.location.hostname}. Developer navigation mode active.`)
-  let navdev = document.getElementsByClassName('navigation-sitemap')[0]
-  let navlist = {
-    href: ['/404.html', '/index.html', '/link.html', '/sitemap.html'],
-    name: ['404', 'Index', 'Link', 'Sitemap'],
-  }
-  let count = 0
-  navdev.innerHTML = ''
-  for (let i = 0; i < navlist.href.length; i++) {
-    navdev.innerHTML += `<li><a href="${navlist.href[i]}">${navlist.name[i]}</a></li>`
-    count++
-  }
-  navdev.innerHTML += `<li>(Count: ${count})</li>`
-}
 /* ===== Other ===== */
