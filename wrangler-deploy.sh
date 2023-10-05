603d04ec70ddb88f26167a9520be9e7efb247454
bydzen <svnteentthones@gmail.com>
2023-10-05 09:58:13 +0700

    deleted sh and txt

--- wrangler-deploy.sh
+++
@@ -1,5 +0,0 @@
-echo "Deploying to: sultankautsar"
-read -p "Enter commit message: " commitMessage
-read -p "Enter directory to deploy: public/" directoryDeploy
-echo "Deploy from directory public/$directoryDeploy with commit message $commitMessage"
-wrangler pages deploy public/$directoryDeploy --project-name "sultankautsar" --commit-message "$commitMessage"
