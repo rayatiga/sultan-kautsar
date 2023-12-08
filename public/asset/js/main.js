9822bdca5a5024da489ee9dcb723fc0885833500
bydzen <svnteentthones@gmail.com>
2023-12-08 13:59:37 +0700

    change ti bg tertiary with custom color

--- public/asset/js/main.js
+++ public/asset/js/main.js
@@ -1,4 +1,3 @@
-// Dynamic main height to minimum height 100dvh
 const main = document.querySelector("main");
 const header = document.querySelector("header");
 const footer = document.querySelector("footer");
@@ -7,32 +6,11 @@ window.addEventListener("resize", () => {
   main.style.minHeight = `calc(100dvh - ${header.offsetHeight}px - ${footer.offsetHeight}px)`;
 });
 
-// Popover trigger
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
 const popoverList = [...popoverTriggerList].map((popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl));
 popoverTriggerList.forEach((popoverTriggerEl) => {
   popoverTriggerEl.style.cursor = "pointer";
 });
 
-// Tooltip trigger
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
-
-// Form validation
-(() => {
-  "use strict";
-  const forms = document.querySelectorAll(".needs-validation");
-  Array.from(forms).forEach((form) => {
-    form.addEventListener(
-      "submit",
-      (event) => {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add("was-validated");
-      },
-      false
-    );
-  });
-})();
