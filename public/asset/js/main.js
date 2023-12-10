a8f96cdd13e86825e7519f421c51a5b08ea23b99
bydzen <svnteentthones@gmail.com>
2023-12-10 15:32:27 +0700

    rearrage and separate the files

--- public/asset/js/main.js
+++ public/asset/js/main.js
@@ -5,12 +5,10 @@ main.style.minHeight = `calc(100dvh - ${header.offsetHeight}px - ${footer.offset
 window.addEventListener("resize", () => {
   main.style.minHeight = `calc(100dvh - ${header.offsetHeight}px - ${footer.offsetHeight}px)`;
 });
-
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
 const popoverList = [...popoverTriggerList].map((popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl));
 popoverTriggerList.forEach((popoverTriggerEl) => {
   popoverTriggerEl.style.cursor = "pointer";
 });
-
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
