0f9852342e6689db44c2e21093835f094ece7904
bydzen <svnteentthones@gmail.com>
2024-04-07 07:27:59 +0700

    Conditional popup showing

--- public/arc/asset/js/main.js
+++ public/arc/asset/js/main.js
@@ -16,8 +16,8 @@ function playSong() {
   audio.volume = 0.9;
   audio.loop = true;
 }
+var popupSong = document.getElementById("popup-song");
 function openPopup() {
-  var popupSong = document.getElementById("popup-song");
   var popupOverlay = document.getElementById("popup-overlay");
   var gifAnimated = document.querySelectorAll(".gif.animated");
   for (let i = 0; i < gifAnimated.length; i++) {
@@ -27,7 +27,9 @@ function openPopup() {
   popupOverlay.style.display = "block";
   document.body.style.overflow = "hidden";
 }
-window.onload = openPopup();
+if (popupSong) {
+  window.onload = openPopup();
+}
 function closePopup() {
   var popupSong = document.getElementById("popup-song");
   var popupOverlay = document.getElementById("popup-overlay");
