c0e73a357ce88e621182b2011a424d2515282c67
bydzen <svnteentthones@gmail.com>
2024-04-19 15:20:51 +0700

    Create worker for kind

--- public/arc/asset/js/diary.js
+++ public/arc/asset/js/diary.js
@@ -1,16 +1,22 @@
 const queryString = window.location.search;
 const params = new URLSearchParams(queryString);
-const paramContent = params.get("about");
+const paramAbout = params.get("about");
+const paramKind = params.get("kind");
 
 const intro = document.getElementById("intro");
 const about = document.getElementById("about");
+const kind = document.getElementById("kind");
 const result = document.getElementById("result");
 const aboutPick = document.getElementById("about-pick");
+const aboutImage = document.getElementById("about-image");
 const quote = document.getElementById("quote");
 const dimmed = document.getElementById("dimmed");
 const character = document.getElementById("character");
 
-if (params.has("about")) {
+const backBtn = document.getElementById("back");
+
+if (params.has("about") && params.get("about") != "back") {
+  aboutImage.style.display = "none";
   about.style.display = "none";
   intro.style.display = "none";
   dimmed.style.display = "none";
@@ -19,19 +25,50 @@ if (params.has("about")) {
   fetch("/arc/asset/json/diary.json")
     .then((response) => response.json())
     .then((data) => {
-      for (let i = 0; i < data.length; i++) {
-        if (paramContent == data[i].about) {
-          aboutPick.innerText = data[i].about;
-          quote.innerText = data[i].content;
+      for (let i = 0; i < data[0].aboutList.length; i++) {
+        if (paramAbout == data[0].aboutList[i].about) {
+          aboutPick.innerText = data[0].aboutList[i].about;
+          quote.innerText = data[0].aboutList[i].content;
           break;
         }
         aboutPick.innerText = "...";
-        quote.innerText = "Aku minta maaf, ini sedang dalam perancangan. Aku harap kamu baik-baik saja.";
+        quote.innerText = "Aku minta maaf, ini sedang dalam perancangan. Stay tune, ya!";
       }
     });
-} else {
+} else if (params.get("about") == "back") {
   about.style.display = "flex";
   about.scrollIntoView();
+  intro.style.display = "flex";
+  dimmed.style.display = "flex";
+  character.style.display = "flex";
+  result.style.display = "none";
+}
+
+if (params.has("kind") && params.get("kind") != "back") {
+  aboutImage.style.display = "block";
+  about.style.display = "none";
+  intro.style.display = "none";
+  dimmed.style.display = "none";
+  character.style.display = "none";
+  result.style.display = "flex";
+  fetch("/arc/asset/json/diary.json")
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < data[1].kindList.length; i++) {
+        if (paramKind == data[1].kindList[i].kind) {
+          aboutPick.innerText = data[1].kindList[i].kind;
+          quote.innerText = data[1].kindList[i].content;
+          aboutImage.setAttribute("src", `/arc/asset/gif/${data[1].kindList[i].image}`)
+          break;
+        }
+        aboutPick.innerText = "...";
+        quote.innerText = "Aku minta maaf, ini sedang dalam perancangan. Stay tune, ya!";
+      }
+    });
+  backBtn.setAttribute("href", "./diary.html?kind=back");
+} else if (params.get("kind") == "back") {
+  about.style.display = "flex";
+  kind.scrollIntoView();
   intro.style.display = "flex";
   dimmed.style.display = "flex";
   character.style.display = "flex";
