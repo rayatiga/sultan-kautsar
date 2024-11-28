666bd5d4d018dca02cf126ea44bada369e6d5fed
bydzen <svnteentthones@gmail.com>
2024-11-28 10:41:52 +0700

    add landing js and css

---
+++ public/asset/media/landing.js
@@ -0,0 +1,4 @@
+function checkLocal() {
+    return window.location.href.includes("127.0.0.1") || window.location.href.includes("localhost") ? true : false;
+}
+checkLocal() && console.log("Message:\nLocal development.\nLog showed only in local mode.");
