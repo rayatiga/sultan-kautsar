cd1e5ae027f2a5f8b98271f0bd3099f87e1f3558
bydzen <svnteentthones@gmail.com>
2025-02-03 13:41:09 +0700

    main script

--- public/asset/js/main.js
+++
@@ -1,8 +0,0 @@
-function openModal() {
-    const modal = document.querySelector(".modal");
-    if (!modal.classList.contains("show")) {
-        modal.classList.add("show");
-    } else {
-        modal.classList.remove("show");
-    }
-}
